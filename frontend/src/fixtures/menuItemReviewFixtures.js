12c940efbf4a9af04c3870f0380a238ce23a7c8c
Shruthi Unnithan <ssanthoshunnithan@ucsb.edu>
2025-05-01 16:10:41 -0700

    fixtures completed

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,38 @@
+const menuItemReviewFixtures = {
+    oneReview: {
+        "id": 6,
+        "itemId": 20,
+        "reviewerEmail": "somwest@gmail.com",
+        "stars": 3,
+        "dateReviewed": "2025-05-01T22:48:12",
+        "comments": "it was alright"
+    },
+    threeReview:[
+        {
+          "id": 6,
+          "itemId": 20,
+          "reviewerEmail": "somwest@gmail.com",
+          "stars": 3,
+          "dateReviewed": "2025-05-01T22:48:12",
+          "comments": "it was alright"
+        },
+        {
+          "id": 7,
+          "itemId": 25,
+          "reviewerEmail": "swaggypomme@gmail.com",
+          "stars": 5,
+          "dateReviewed": "2025-05-24T11:40:00",
+          "comments": "good price, great taste"
+        },
+        {
+          "id": 8,
+          "itemId": 30,
+          "reviewerEmail": "eding42@gmail.com",
+          "stars": 1,
+          "dateReviewed": "2025-03-01T07:48:00",
+          "comments": "might be the worst thing i've ever put in my mouth"
+        }
+      ] ,
+  };
+  
+  export { menuItemReviewFixtures };
