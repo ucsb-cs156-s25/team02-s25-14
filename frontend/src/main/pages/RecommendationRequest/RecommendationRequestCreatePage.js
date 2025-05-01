fb9283bf575fd6bbd9d51bfdb45582fb05f12c2e
chloeandersen-dev <chloe@andersens.com>
2025-05-01 10:58:37 -0700

    fixing 500 error, adding seconds to date picker

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.js
@@ -33,8 +33,15 @@ export default function RecommendationRequestCreatePage({ storybook = false }) {
 
   const { isSuccess } = mutation;
 
+  const fixDateFormat = (str) => (str.length === 16 ? `${str}:00` : str); // adding :00 to the end of the string to make it a valid date
+
   const onSubmit = async (data) => {
-    mutation.mutate(data);
+    mutation.mutate({
+      ...data,  
+      dateRequested: fixDateFormat(data.dateRequested),
+      dateNeeded: fixDateFormat(data.dateNeeded),
+      done: String(data.done),
+    });
   };
 
   if (isSuccess && !storybook) {
