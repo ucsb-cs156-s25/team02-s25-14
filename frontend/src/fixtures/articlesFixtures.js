e2015aafda687b9bf7006c4631a32de6ebf7c19d
Karen Yuan <karenyuan@ucsb.edu>
2025-04-30 17:10:50 -0700

    karenlyuan - create fixtures for articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "Ranking UCSB’s most vegetarian-friendly dining halls",
+        "url": "https://dailynexus.com/2023-08-24/ranking-ucsbs-most-vegetarian-friendly-dining-halls/",
+        "explanation": "Daily Nexus article 1",
+        "email": "karenyuan@ucsb.edu",
+        "dateAdded": "2023-08-24T09:00:00"
+    },
+    threeArticles: [
+      {
+        "id": 1,
+        "title": "Ranking UCSB’s most vegetarian-friendly dining halls",
+        "url": "https://dailynexus.com/2023-08-24/ranking-ucsbs-most-vegetarian-friendly-dining-halls/",
+        "explanation": "Daily Nexus article 1",
+        "email": "karenyuan@ucsb.edu",
+        "dateAdded": "2023-08-24T09:00:00"
+      },
+      {
+        "id": 3,
+        "title": "What food is offered at the UCSB dining halls?",
+        "url": "https://dailynexus.com/2025-02-28/what-food-is-offered-at-the-ucsb-dining-halls/",
+        "explanation": "Daily Nexus article 2",
+        "email": "karenyuan@ucsb.edu",
+        "dateAdded": "2025-02-28T09:00:00"
+      },
+      {
+        "id": 4,
+        "title": "Dining Halls Ranked by Someone Who Hasn’t Been to Any of Them",
+        "url": "https://dailynexus.com/2021-04-27/dining-halls-ranked-by-someone-who-hasnt-been-to-any-of-them/",
+        "explanation": "Daily Nexus article 3",
+        "email": "karenyuan@ucsb.edu",
+        "dateAdded": "2021-04-27T09:00:00"
+      }
+    ]
+  };
+  
+  export { articlesFixtures };
+  
