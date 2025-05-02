ce65028b181470d5b98edd7d365b538399bda451
Ryan Choi <ryanchoi@169-231-70-76.wireless.ucsb.edu>
2025-05-02 12:06:40 -0700

    r - WIP forms/stories, TBD tests

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneUCSBDiningCommonsMenuItem: {
+      id: 1,
+      diningCommonsCode: "A1",
+      name: "Sauce",
+      station: "Portola",
+    },
+    threeUCSBDiningCommonsMenuItems: [
+      {
+        id: 2,
+        diningCommonsCode: "B1",
+        name: "Boss",
+        station: "Portola",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "C3",
+        name: "Crawfish",
+        station: "Portola",
+      },
+      {
+        id: 4,
+        diningCommonsCode: "D4",
+        name: "Mate",
+        station: "Portola",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
