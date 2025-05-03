033d46a277074ab81371f9f7260256e646f53b8d
Andrew Zhang <chi_zhang952@ucsb.edu>
2025-05-02 18:04:32 -0700

    az-fix formatting

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,31 +1,30 @@
 const ucsbOrganizationFixtures = {
-    oneUCSBOrganization: {
-      orgCode: "RHA",
-      orgTranslationShort: "Res Hall Assoc",
-      orgTranslation: "Residence Halls Association",
+  oneUCSBOrganization: {
+    orgCode: "RHA",
+    orgTranslationShort: "Res Hall Assoc",
+    orgTranslation: "Residence Halls Association",
+    inactive: false,
+  },
+  threeUCSBOrganizations: [
+    {
+      orgCode: "AS",
+      orgTranslationShort: "Assoc Students",
+      orgTranslation: "Associated Students",
       inactive: false,
     },
-    threeUCSBOrganizations: [
-      {
-        orgCode: "AS",
-        orgTranslationShort: "Assoc Students",
-        orgTranslation: "Associated Students",
-        inactive: false,
-      },
-      {
-        orgCode: "ASBS",
-        orgTranslationShort: "AS Bike Shop",
-        orgTranslation: "Associated Students Bike Shop",
-        inactive: true,
-      },
-      {
-        orgCode: "ARC",
-        orgTranslationShort: "Asian Resource Center",
-        orgTranslation: "Asian Resource Center",
-        inactive: false,
-      },
-    ],
-  };
-  
-  export { ucsbOrganizationFixtures };
-  
+    {
+      orgCode: "ASBS",
+      orgTranslationShort: "AS Bike Shop",
+      orgTranslation: "Associated Students Bike Shop",
+      inactive: true,
+    },
+    {
+      orgCode: "ARC",
+      orgTranslationShort: "Asian Resource Center",
+      orgTranslation: "Asian Resource Center",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
