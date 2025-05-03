cd6d20e05879ede702efd0eb83efc538321856be
Shruthi Unnithan <ssanthoshunnithan@ucsb.edu>
2025-05-03 11:44:31 -0700

    adding placeholder pages for menu item review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -88,6 +88,7 @@ function App() {
               path="/menuitemreview"
               element={<MenuItemReviewIndexPage />}
             />
+            <Route exact path="/menuitemreview" element={<MenuItemReviewIndexPage />} />
           </>
         )}
         {hasRole(currentUser, "ROLE_ADMIN") && (
