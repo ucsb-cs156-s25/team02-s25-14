941f5f032053e3e734294c736210e308f8187c2a
chloeandersen-dev <chloe@andersens.com>
2025-05-05 09:20:04 -0700

    fixing nav bar not showing all data tables

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -66,14 +66,26 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
-                    UCSB Dining Commons Menu Item
-                  </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
+                    UCSB Dining Commons Menu Item
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/helprequests">
-                    HelpRequest
+                    Help Request
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/recommendationrequests">
+                    Recommendation Requests
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/articles">
+                    Articles
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/ucsborganizations">
+                    UCSB Organizations
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/menuitemreview">
+                    Menu Item Reviews
                   </Nav.Link>
                 </>
               ) : (
