9e12e1b724545a2ab9bff30f4ad5c63a825a0c25
Andrew Zhang <chi_zhang952@ucsb.edu>
2025-05-06 21:29:37 -0700

    az-fix merge conflict

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -78,11 +78,11 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/articles">
                     Articles
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/menuitemreview">
-                    MenuItemReview
-                  </Nav.Link>
                   <Nav.Link as={Link} to="/ucsborganization">
                     UCSB Organizations
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/menuitemreview">
+                    MenuItemReview
                   </Nav.Link>
                 </>
               ) : (
