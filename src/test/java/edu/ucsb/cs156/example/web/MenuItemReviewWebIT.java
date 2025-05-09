eaf301157de8f310a67809ffcc08898db1d23ea6
Shruthi Unnithan <ssanthoshunnithan@ucsb.edu>
2025-05-09 10:39:23 -0700

    end2end testing implemented

---
+++ src/test/java/edu/ucsb/cs156/example/web/MenuItemReviewWebIT.java
@@ -0,0 +1,29 @@
+package edu.ucsb.cs156.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import edu.ucsb.cs156.example.WebTestCase;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class MenuItemReviewWebIT extends WebTestCase {
+    @Test
+    public void regular_user_cannot_create_review() throws Exception {
+        setupUser(false);
+
+        page.getByText("Menu Item Reviews").click();
+
+        assertThat(page.getByText("Create Menu Item Review")).not().isVisible();
+        assertThat(page.getByTestId("MenuItemReviewTable-cell-row-0-col-name")).not().isVisible();
+    }
+}
