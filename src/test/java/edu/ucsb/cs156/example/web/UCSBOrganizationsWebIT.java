4d7f4cb2b078ec22c78b110a9430af6e4248fa47
Chloe Andersen <chloeandersen@ucsb.edu>
2025-05-09 17:52:56 -0700

    ucsb org end to end tests passing

---
+++ src/test/java/edu/ucsb/cs156/example/web/UCSBOrganizationsWebIT.java
@@ -0,0 +1,33 @@
+package edu.ucsb.cs156.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import edu.ucsb.cs156.example.WebTestCase;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class UCSBOrganizationsWebIT extends WebTestCase {
+    
+    @Test
+    public void regular_user_cannot_create_organization() throws Exception {
+        setupUser(false);
+
+        page.getByText("UCSB Organizations").click();
+        page.waitForURL("**/ucsborganization");
+
+        page.waitForSelector("text=UCSB Organizations");
+        
+        assertThat(page.getByText("Create UCSB Organization")).not().isVisible();
+        assertThat(page.getByTestId("UCSBOrganizationTable-cell-row-0-col-orgCode")).not().isVisible();
+    }
+}
