300bfbe8078067c4f1c6fe2fe03ce77e4d785a9e
Chloe Andersen <chloeandersen@ucsb.edu>
2025-05-09 17:41:47 -0700

    testing ucsb integration tests

---
+++ src/test/java/edu/ucsb/cs156/example/integration/UCSBOrganizationsIT.java
@@ -0,0 +1,93 @@
+package edu.ucsb.cs156.example.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.hamcrest.Matchers.containsString;
+
+import edu.ucsb.cs156.example.entities.UCSBOrganizations;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationsRepository;
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.services.CurrentUserService;
+import edu.ucsb.cs156.example.services.GrantedAuthoritiesService;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+
+import java.time.LocalDateTime;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+@Import(TestConfig.class)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+
+/*
+    This is a Integration Test class for the UCSBOrganizations controller.
+*/
+
+public class UCSBOrganizationsIT {
+    @Autowired
+    UCSBOrganizationsRepository ucsbOrganizationsRepository;
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void get_ucsborganizations() throws Exception {
+        ucsbOrganizationsRepository.deleteAll();
+
+        // arrange
+        UCSBOrganizations ucsbOrganization = UCSBOrganizations.builder()
+            .orgCode("UCSB")
+            .orgTranslationShort("UCSB")
+            .orgTranslation("University of California, Santa Barbara")
+            .build();
+                        
+        ucsbOrganizationsRepository.save(ucsbOrganization);
+
+        mockMvc.perform(get("/api/ucsborganizations/all"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("UCSB")))
+            .andExpect(content().string(containsString("University of California, Santa Barbara")));
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void post_ucsborganizations() throws Exception {
+        MvcResult response = mockMvc.perform(
+            post("/api/ucsborganizations/post")
+                .param("orgCode", "UCSB")
+                .param("orgTranslationShort", "UCSB")
+                .param("orgTranslation", "University of California, Santa Barbara")
+                .with(csrf()))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("UCSB")))
+            .andExpect(content().string(containsString("University of California, Santa Barbara")))
+            .andReturn();
+
+        String responseString = response.getResponse().getContentAsString();
+
+        assert(responseString.contains("UCSB"));
+        assert(responseString.contains("University of California, Santa Barbara"));
+    }
+}
